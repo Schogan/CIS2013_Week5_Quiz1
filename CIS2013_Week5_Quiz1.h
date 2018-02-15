6707b6da2b19da27e96dd32e46a23ec04b73f059
Schogan <navy15@gmail.com>
2018-02-14 18:17:47 -0600

    initial

---
+++ CIS2013_Week5_Quiz1.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+int first_num = 0;
+int secon_num = 0;
+int prime = 0;
+int i = 0;
+
+int prime_num(first_num,secon_num){
+	for (i=first_num;i<secon_num/2;i++){
+		
+	}
+	if (prime==0){
+			cout << prime;
+		}
+}
+
+int lc_denominator(){
+	
+}
+
+int main(){
+	
+	
+	cout << "Please enter your first number: " << endl;
+	cin >> first_num;
+	cout << "Please enter your second number: " << endl;
+	cin >> secon_num;
+	
+	prime_num(first_num,secon_num);
+	lc_denominator(first_num,secon_num);
+}
